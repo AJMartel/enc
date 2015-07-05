aec21e92ad6ec11855773c5f317869d1ce545937
Alain Martel <dev@martel.srsly.ca>
2015-07-04 22:16:52 -0400

    Update mktarball.sh

--- mktarball.sh
+++ mktarball.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-name="dbd"
-version="`cat dbd.c | grep '$Id: dbd.c,v' | cut -d' ' -f3`"
-files="dbd.c dbd.h doexec.c pel.c aes.c sha1.c socket_code.h pel.h aes.h sha1.h doexec_unix.h doexec_win32.h readwrite.h misc.h Makefile mktarball.sh README COPYING CHANGES binaries"
+name="enc"
+version="`cat $name.c | grep '$Id: '$name'.c,v' | cut -d' ' -f3`"
+files="$name $name.c $name.h aes.c aes.h doexec.c doexec_unix.h doexec_win32.h pel.c pel.h readwrite.h sha1.c sha1.h socket_code.h misc.h Makefile mktarball.sh README COPYING CHANGES"
 
 echo "[i] making tarball..."
 mkdir $name-$version
@@ -14,4 +14,3 @@ rm -rf $name-$version
 rm -rf temporaryplace-$version.tar
 chmod 0644 $name-$version.tar.gz
 echo "[i] done"
-
